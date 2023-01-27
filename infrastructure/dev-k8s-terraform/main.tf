e61a4f5b8ad16e6a4364d0005f24795e3fac66ba
Iyigit03 <ihsanyigit2017@gmail.com>
2023-01-27 15:30:16 +0000

    xd

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -157,8 +157,8 @@ resource "aws_instance" "kube-master" {
     iam_instance_profile = module.iam.master_profile_name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-    subnet_id = "subnet-021cb70bed9877623"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1b"
+    subnet_id = "subnet-0559951de97642128"  # select own subnet_id of us-east-1a
+    availability_zone = "us-east-1a"
     tags = {
         Name = "kube-master"
         "kubernetes.io/cluster/petclinicCluster" = "owned"
@@ -175,7 +175,7 @@ resource "aws_instance" "worker-1" {
         iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-    subnet_id = "subnet-021cb70bed9877623"  # select own subnet_id of us-east-1a
+    subnet_id = "subnet-0df61fdbfda601e40"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
         Name = "worker-1"
@@ -193,7 +193,7 @@ resource "aws_instance" "worker-2" {
     iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
     key_name = "clarus"
-    subnet_id = "subnet-021cb70bed9877623"  # select own subnet_id of us-east-1a
+    subnet_id = "subnet-0c0daef84689821f4"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
         Name = "worker-2"
